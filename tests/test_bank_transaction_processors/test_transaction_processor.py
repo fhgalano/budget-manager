1589e39b348d9b03f22ea3f66f00133bde499bec
fhgalano <fhgalano@gmail.com>
2022-08-15 21:24:57 -0500

    tested (most of) the parts of the processor that don't require bank specific function overwrites

--- tests/test_bank_transaction_processors/test_transaction_processor.py
+++ tests/test_bank_transaction_processors/test_transaction_processor.py
@@ -1,21 +1,26 @@
-#import pytest
+import pytest
 import os
-from os import getenv
+
 from dotenv import load_dotenv
+from pandas import read_csv
+from sqlite3 import Error
 
 from bank_transaction_processor.transaction_processor import Processor
-from definitions import ROOT_DIR
+from definitions import ROOT_DIR, TEST_RESOURCE_DIR
 
 env_path = ROOT_DIR / '.env'
 load_dotenv(env_path)
 
-#@pytest.fixture
+
+@pytest.fixture
 def processor():
     return Processor(
         os.getenv('DB_URL'),
         ROOT_DIR / 'tests/resources/test_transactions.csv'
     )
 
+
+@pytest.fixture
 def fake_processor():
     return Processor(
         'fake_url',
@@ -23,15 +28,41 @@ def fake_processor():
     )
 
 
+@pytest.fixture()
+def transaction_df():
+    return read_csv(TEST_RESOURCE_DIR / 'test_transactions.csv')
+
+
 def test_open_db(processor, fake_processor):
     processor.open_db_connection()
-    fake_processor.open_db_connection()
-    #TODO: The connection creates an empty db when given a url that doesn't exist
+    try:
+        fake_processor.open_db_connection()
+    except Error as e:
+        print(f"successfully caught error: {e}")
+        assert True
 
     assert processor.db_connection is not None
     assert fake_processor.db_connection is None
 
 
-if __name__ == '__main__':
-    test_open_db(processor(), fake_processor())
+def test_close_db(processor):
+    processor.open_db_connection()
+    processor.close_db_connection()
+
+    assert processor.db_connection is None
+
+
+def test_get_budget_category():
+    # TODO: Finish
+    pass
+
+
+def test_clean_up_seller_name(processor, transaction_df):
+    test_cases = transaction_df.Description.tolist()
+
+    # TODO: Make the test have an actual assert
+    for seller_name in test_cases:
+        print(processor._clean_up_seller_name(seller_name))
+
+
 
